7826b8f2938a58057fba142664bfd4170cb07c17
s54197 <azri.mohamad@tm.com.my>
2021-08-04 21:37:01 +0800

    add test admin page and custom admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,25 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext as _   # convert string to human readable text
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),  # None is section
+        (_('Personal Info'), {'fields': ('name',)}),    # after 'name' need a , to differentiate with string
+        (_('Permissions'),{'fields': ('is_active','is_staff','is_superuser',)}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, 
+        {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
